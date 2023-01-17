27daef26a563e79941415ebc54ecd4fa14cd4c06
JhonatanSoto <sotojhonatan897@gmail.com>
2023-01-17 20:01:24 -0300

    actulizando archivos

--- app.js
+++ app.js
@@ -2,17 +2,10 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const publicPath = path.resolve(__dirname, './public');
-const PORT = 3000
+const PORT = process.env.PORT||3000
 
 app.use(express.static(publicPath));
 app.get('/', (req, res)=> res.sendFile(path.resolve(__dirname, './views/home.html')))
 app.get('/register', (req, res)=> res.sendFile(path.resolve(__dirname, './views/register.html')))
 app.get('/login', (req, res)=> res.sendFile(path.resolve(__dirname, './views/login.html')))
-
-
-
-
-
-
-
 app.listen(PORT,()=>{console.log(`Servidor corriendo en el puerto ${PORT}`)});
